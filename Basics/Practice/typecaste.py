cbd763d2266fb0c7a45fb80a516a1320289d0841
sarankumarPrakash <sarankumar1317@gmail.com>
2025-08-28 03:51:39 +0530

    type caste

---
+++ Basics/Practice/typecaste.py
@@ -0,0 +1,26 @@
+name="Brocode"
+age=20
+gpa=3.5 
+isMale=True
+
+
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(isMale))
+
+
+#  type casting
+
+age=float(age)  # convert int to float
+gpa=int(gpa)    # convert float to int
+isMale=str(isMale) # convert bool to str
+name=bool(name)  # convert str to bool
+
+
+print(age)
+print(gpa)
+print(isMale)
+print(name)
+
