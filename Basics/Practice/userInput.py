14966459e6933ee3737b94163bf673c78d174b2c
sarankumarPrakash <sarankumar1317@gmail.com>
2025-08-28 03:55:09 +0530

    user Input

---
+++ Basics/Practice/userInput.py
@@ -0,0 +1,11 @@
+name= input("Enter your name : ")
+age= input("Enter your age : ")
+gpa= input("Enter your gpa : ")
+
+
+
+print("--------------------------------------------")
+print("Hello " + name)
+print("You are " + age + " years old")  
+print("Your gpa is " + gpa)    
+print("--------------------------------------------")
